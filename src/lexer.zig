ac432fae234d58de871522c00d62069526517706
Kien Nguyen <kiennt23@gmail.com>
2026-07-03 16:30:49 +0700

    feat: keywords and identifiers

--- src/lexer.zig
+++ src/lexer.zig
@@ -4,6 +4,17 @@ const TokenType = token.TokenType;
 const std = @import("std");
 const testing = std.testing;
 
+const keywords = std.StaticStringMap(TokenType).initComptime(.{
+    .{ "type", TokenType.kw_type },
+    .{ "relation", TokenType.kw_relation },
+    .{ "permission", TokenType.kw_permission },
+    .{ "condition", TokenType.kw_condition },
+    .{ "with", TokenType.kw_with },
+    .{ "in", TokenType.kw_in },
+    .{ "self", TokenType.kw_self },
+    .{ "resource", TokenType.kw_resource },
+});
+
 pub const Lexer = struct {
     input: []const u8,
     pos: usize,
@@ -161,9 +172,23 @@ pub const Lexer = struct {
                     return t;
                 }
             },
-            else => {
-                t.type = TokenType.illegal;
-            },
+            else => {},
+        }
+        return t;
+    }
+
+    fn isAlpha(self: *Lexer) bool {
+        return (self.ch >= 'a' and self.ch <= 'z') or (self.ch >= 'A' and self.ch <= 'Z');
+    }
+
+    fn readIdentifierToken(self: *Lexer) Token {
+        var t = Token{ .type = TokenType.identifier, .start = self.pos, .end = self.pos };
+        while (self.isAlpha()) {
+            self.readChar();
+        }
+        t.end = self.pos;
+        if (keywords.get(self.input[t.start..t.end])) |kw| {
+            t.type = kw;
         }
         return t;
     }
@@ -187,7 +212,10 @@ pub const Lexer = struct {
                 t = self.readLookaheadCharToken();
             },
             else => {
-                t.type = TokenType.illegal;
+                if (self.isAlpha()) {
+                    t = self.readIdentifierToken();
+                    return t;
+                }
             },
         }
         self.readChar();
@@ -262,6 +290,79 @@ test "lookahead tokens same first char consecutive" {
         Token{ .type = TokenType.range, .start = 15, .end = 17 },
         Token{ .type = TokenType.dot, .start = 18, .end = 19 },
         Token{ .type = TokenType.eof, .start = 19, .end = 19 },
+    };
+    var i: usize = 0;
+    while (i < expected.len) : (i += 1) {
+        const t = lexer.next();
+        try testing.expectEqual(expected[i].type, t.type);
+        try testing.expectEqual(expected[i].start, t.start);
+        try testing.expectEqual(expected[i].end, t.end);
+    }
+}
+
+test "keywords" {
+    const input = "type relation permission condition with in self resource";
+    var lexer = Lexer.init(input);
+    const expected = [_]Token{
+        Token{ .type = TokenType.kw_type, .start = 0, .end = 4 },
+        Token{ .type = TokenType.kw_relation, .start = 5, .end = 13 },
+        Token{ .type = TokenType.kw_permission, .start = 14, .end = 24 },
+        Token{ .type = TokenType.kw_condition, .start = 25, .end = 34 },
+        Token{ .type = TokenType.kw_with, .start = 35, .end = 39 },
+        Token{ .type = TokenType.kw_in, .start = 40, .end = 42 },
+        Token{ .type = TokenType.kw_self, .start = 43, .end = 47 },
+        Token{ .type = TokenType.kw_resource, .start = 48, .end = 56 },
+        Token{ .type = TokenType.eof, .start = 56, .end = 56 },
+    };
+    var i: usize = 0;
+    while (i < expected.len) : (i += 1) {
+        const t = lexer.next();
+        try testing.expectEqual(expected[i].type, t.type);
+        try testing.expectEqual(expected[i].start, t.start);
+        try testing.expectEqual(expected[i].end, t.end);
+    }
+}
+
+test "identifiers" {
+    const input = "foo bar baz";
+    var lexer = Lexer.init(input);
+    const expected = [_]Token{
+        Token{ .type = TokenType.identifier, .start = 0, .end = 3 },
+        Token{ .type = TokenType.identifier, .start = 4, .end = 7 },
+        Token{ .type = TokenType.identifier, .start = 8, .end = 11 },
+        Token{ .type = TokenType.eof, .start = 11, .end = 11 },
+    };
+    var i: usize = 0;
+    while (i < expected.len) : (i += 1) {
+        const t = lexer.next();
+        try testing.expectEqual(expected[i].type, t.type);
+        try testing.expectEqual(expected[i].start, t.start);
+        try testing.expectEqual(expected[i].end, t.end);
+    }
+}
+
+test "simple type" {
+    const input =
+        \\type Team {
+        \\    relation member: User
+        \\    permission members = member
+        \\}
+    ;
+    var lexer = Lexer.init(input);
+    const expected = [_]Token{
+        Token{ .type = TokenType.kw_type, .start = 0, .end = 4 },
+        Token{ .type = TokenType.identifier, .start = 5, .end = 9 },
+        Token{ .type = TokenType.l_brace, .start = 10, .end = 11 },
+        Token{ .type = TokenType.kw_relation, .start = 16, .end = 24 },
+        Token{ .type = TokenType.identifier, .start = 25, .end = 31 },
+        Token{ .type = TokenType.colon, .start = 31, .end = 32 },
+        Token{ .type = TokenType.identifier, .start = 33, .end = 37 },
+        Token{ .type = TokenType.kw_permission, .start = 42, .end = 52 },
+        Token{ .type = TokenType.identifier, .start = 53, .end = 60 },
+        Token{ .type = TokenType.assign, .start = 61, .end = 62 },
+        Token{ .type = TokenType.identifier, .start = 63, .end = 69 },
+        Token{ .type = TokenType.r_brace, .start = 70, .end = 71 },
+        Token{ .type = TokenType.eof, .start = 71, .end = 71 },
     };
     var i: usize = 0;
     while (i < expected.len) : (i += 1) {

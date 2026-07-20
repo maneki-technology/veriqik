07d7a6d3c08ed333d335eab210a50f126ac7dbbe
Kien Nguyen <kiennt23@gmail.com>
2026-07-20 18:48:41 +0700

    feat(model parser): parse condition

--- src/model/ast.zig
+++ src/model/ast.zig
@@ -20,13 +20,38 @@ pub const Type = struct {
     span: Span,
 };
 
-pub const Condition = struct {};
+pub const ValueTypeRef = struct {
+    name: Identifier,
+    span: Span,
+    collection: bool,
+};
+
+pub const Parameter = struct {
+    name: Identifier,
+    type: ValueTypeRef,
+    span: Span,
+};
+
+pub const Condition = struct {
+    name: Identifier,
+    params: []const Parameter = &.{},
+    span: Span,
+
+    pub fn deinit(self: *const Condition, allocator: std.mem.Allocator) void {
+        allocator.free(self.params);
+    }
+};
 
 pub const Model = struct {
     types: []const Type = &.{},
+    conditions: []const Condition = &.{},
 
     pub fn deinit(self: *Model, allocator: std.mem.Allocator) void {
         allocator.free(self.types);
+        for (self.conditions) |*cond| {
+            cond.deinit(allocator);
+        }
+        allocator.free(self.conditions);
         self.* = undefined;
     }
 };
